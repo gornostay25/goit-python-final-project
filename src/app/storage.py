8c2a1a84e7395028aeebb47c8838305363fa9e24
slipkoliudmyla <slipkoliudmila@gmail.com>
2026-03-11 15:49:34 +0200

    Implement data storage module (#4) (#9)
    * Implement data storage module (#4)
    
    * style: format storage module with ruff
    
    ---------
    
    Co-authored-by: Liudmyla Slipko <LiudmylaS@oddity.com>

---
+++ src/app/storage.py
@@ -0,0 +1,30 @@
+import json
+import os
+
+
+# Назви файлів для збереження даних
+CONTACTS_FILE = "contacts.json"
+NOTES_FILE = "notes.json"
+
+
+def save_data(filename, data):
+    """
+    Зберігає дані у JSON-файл.
+    """
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
+
+
+def load_data(filename):
+    """
+    Завантажує дані з JSON-файлу.
+    Якщо файл не існує або пошкоджений, повертає порожній список.
+    """
+    if not os.path.exists(filename):
+        return []
+
+    with open(filename, "r", encoding="utf-8") as file:
+        try:
+            return json.load(file)
+        except json.JSONDecodeError:
+            return []
